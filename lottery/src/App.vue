f06e5d46e31cf68b07558fb64dc42b30202a1103
jie-L <2366912064@qq.com>
2019-09-18 08:49:11 +0800

    --

--- lottery/src/App.vue
+++ lottery/src/App.vue
@@ -1,31 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  created() {
+    this.$axios
+      .get("http://elm.cangdu.org/v1/pois?type=nearby", {
+        params: {
+          keyword: '北京'
+        }
+      })
+      .then(data => {
+        console.log(data);
+      });
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+*{
+  margin: 0;padding: 0;
 }
 </style>
--- lottery/src/main.js
+++ lottery/src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'lib-flexible/flexible'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios,axios)
+
+axios.defaults.withCredentials = true
+Vue.prototype.$axios = axios;
 
 new Vue({
   router,
   store,
+  axios,
+  VueAxios,
   render: h => h(App)
 }).$mount('#app')
